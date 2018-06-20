83ebb795273e8aa44abde07a5eb1e3d9d0dae84b
KamuiRoeru <me1802@s.akashi.ac.jp>
2018-06-20 16:19:02 +0900

    [fix] fix

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 
+
 def main(argv):
-    A = np.arange(10).reshape(2,5)
-    print(A)
+    # A = np.arange(10).reshape(2,5)
+    # print(A)
     print('Hello {}!'.format(argv[0]))
